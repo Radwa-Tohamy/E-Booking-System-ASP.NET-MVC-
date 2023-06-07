1ffac530055a5ce774a2ef9f19be2e4a7a20783b
Radwa-Tohamy <radwamostafa597@gmail.com>
2023-06-08 00:05:28 +0300

    Add project files.

---
+++ E-Booking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Booking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
